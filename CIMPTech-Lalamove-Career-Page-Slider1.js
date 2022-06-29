2faa0d33facc0b7fe996bd96cf3505b9e02c3cfa
CIMP Tech <99329136+cimptechhk@users.noreply.github.com>
2022-06-29 11:58:54 +0800

    Create CIMPTech-Lalamove-Career-Page-Slider1.js

---
+++ CIMPTech-Lalamove-Career-Page-Slider1.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+    showSlides();
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("slide");
+      let dots = document.getElementsByClassName("dot1");
+      console.log(slides);
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      
+      if (slideIndex > 2) {slideIndex = 1;}
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("slider-active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " slider-active";
+      setTimeout(showSlides, 5000); // Change image every 2 seconds
+    }
